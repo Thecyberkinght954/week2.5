8f105520723fcd0077419a17ac195f558e0aea9a
kali <cliffordjones954@icloud.com>
2026-04-18 07:23:25 -0400

    first commit

---
+++ week2.py
@@ -0,0 +1,13 @@
+
+name = 'clifford'
+name2 = 'Jones'
+print (name)
+age = 17
+hight = 1.75
+is_student = True
+
+print("==========          Student Information          ========== ")
+print("My name is ", name, name2)
+print("My age is,", age)
+print ("My hight is,", hight)
+print ("I am a student" ,is_student)
